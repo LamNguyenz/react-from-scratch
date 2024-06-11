fb0e7ead45feb1ce827a8a15d63f408125d0e33c
thanhlam <lam.nguyen@pascal.studio>
2024-06-12 03:20:22 +0700

    feat: add jsx

---
+++ scripts/rollup/react.config.js
@@ -0,0 +1,50 @@
+// @ts-nocheck
+import generatePackageJson from 'rollup-plugin-generate-package-json';
+import { getBaseRollupPlugins, getPackageJSON, resolvePkgPath } from './utils';
+
+const { name, module } = getPackageJSON('react');
+const pkgPath = resolvePkgPath(name);
+const pkgDistPath = resolvePkgPath(name, true);
+
+export default [
+	{
+		input: `${pkgPath}/${module}`,
+		output: {
+			file: `${pkgDistPath}/index.js`,
+			name: 'React',
+			format: 'umd',
+		},
+		plugins: [
+			...getBaseRollupPlugins(),
+			generatePackageJson({
+				inputFolder: pkgPath,
+				outputFolder: pkgDistPath,
+				baseContents: ({ name, description, version }) => ({
+					name,
+					description,
+					version,
+					main: 'index.js',
+				}),
+			}),
+		],
+	},
+	// jsx-runtime
+	{
+		input: `${pkgPath}/src/jsx.ts`,
+		output: [
+			// jsx-runtime
+			{
+				file: `${pkgDistPath}/jsx-runtime.js`,
+				name: 'jsx-runtime',
+				format: 'umd',
+			},
+			// jsx-dev-runtime
+			{
+				file: `${pkgDistPath}/jsx-dev-runtime.js`,
+				name: 'jsx-dev-runtime',
+				format: 'umd',
+			},
+		],
+		plugins: getBaseRollupPlugins(),
+	},
+];
